0caa2e181a4503b72af66b9e8b6da8f4d04d9922
amammele <amammele@gmail.com>
2020-10-06 22:36:43 -0500

    first commit

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenDoor : MonoBehaviour {
+
+    public GameObject OpenPanel = null;
+
+    private bool _isInsideTrigger = false;
+
+    public Animator _animator;
+
+    public string OpenText = "Press 'E' to open";
+
+    public string CloseText = "Press 'E' to close";
+
+    private bool _isOpen = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = true;
+            OpenPanel.SetActive(true);
+            UpdatePanelText();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = false;
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    private void UpdatePanelText() 
+    {
+
+        Text panelText = OpenPanel.transform.Find("Text").GetComponent<Text>();
+        if (panelText != null)
+        {
+            panelText.text = _isOpen ? CloseText : OpenText;
+        }
+    }
+
+    private void Update()
+    {
+        if (IsOpenPanelActive && _isInsideTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _isOpen = !_isOpen;
+
+                Invoke("UpdatePanelText", 1.0f);
+
+                _animator.SetBool("open", _isOpen);
+            }
+        }
+    }
+}
